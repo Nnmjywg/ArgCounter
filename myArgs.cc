56896aad140370e8af2a86d419cf71c7ebd00a45
Nnmjywg <30538769+Nnmjywg@users.noreply.github.com>
2017-07-29 01:10:29 +0200

    Update myArgs.cc

--- myArgs.cc
+++ myArgs.cc
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
 	std::cout << "There are " << argc  << " arguments" << std::endl;
 	for (int count = 0; count < argc; ++count) {
-		std::cout << count << argv[count] << std::endl;
+		std::cout << count << " " << argv[count] << std::endl;
 	}
 	return 0;
 }
