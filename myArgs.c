8bf12b59c743ab0ef5ff9d2eaa72bc84042eacf4
Nnmjywg <30538769+Nnmjywg@users.noreply.github.com>
2017-07-29 09:30:28 +0200

    Update myArgs.c

--- myArgs.c
+++ myArgs.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
 	printf("There are %d arguments\n", argc);
 	for (int count = 0; count < argc; ++count) {
-		printf("%d %s\n", count, argv[count]);
+		printf("%d\t%s\n", count, argv[count]);
 	}
 	return 0;
 }
